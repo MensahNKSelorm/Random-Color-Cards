1d4ca0ccf9d2f05f3949189ec2c253b1bf373697
MensahNKSelorm <mensahnkselorm@gmail.com>
2025-01-07 13:42:07 +0000

    added functionality to copy and check svg

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 let cards = document.querySelectorAll('div.card-1, div.card-2, div.card-3, div.card-4');
-let img = document.createElement('img');
-let link = document.createElement('a')
-img.src = '/assets/copy.svg'
 
 function random(number) {
     return Math.floor(Math.random() * (number + 1));
@@ -15,9 +12,19 @@ function changeCardBackground() {
     cards.forEach((card) => {
         card.style.backgroundColor = `rgb(${random(255)},${random(255)},${random(255)})`
         card.textContent = randomColor();
-        card.append(link)
+        const textContainer = document.createElement('div')
+        textContainer.classList.add('textContainer')
+        const img = document.createElement('img');
+        const link = document.createElement('a')
+        img.src = '/assets/copy.svg'
+        card.append(textContainer)
         link.append(img)
-        console.log(card.innerHTML)
+        textContainer.append(link)
+        img.addEventListener('click', () => {
+            navigator.clipboard.writeText(card.textContent)
+            .then(() => {img.src="/assets/check.svg"}).then(setTimeout(() => {img.src="/assets/copy.svg"}, 3000))
+        })
+
     })
 }
 
