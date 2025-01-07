c7fc36c893069d4359adffc401111c5ef313ff66
MensahNKSelorm <mensahnkselorm@gmail.com>
2025-01-07 13:06:46 +0000

    adding copy button

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let cards = document.querySelectorAll('div.card-1, div.card-2, div.card-3, div.card-4');
+let img = document.createElement('img');
+let link = document.createElement('a')
+img.src = '/assets/copy.svg'
 
 function random(number) {
     return Math.floor(Math.random() * (number + 1));
@@ -12,6 +15,9 @@ function changeCardBackground() {
     cards.forEach((card) => {
         card.style.backgroundColor = `rgb(${random(255)},${random(255)},${random(255)})`
         card.textContent = randomColor();
+        card.append(link)
+        link.append(img)
+        console.log(card.innerHTML)
     })
 }
 
